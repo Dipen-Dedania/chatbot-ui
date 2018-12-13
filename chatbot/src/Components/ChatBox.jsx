b0e0bcc61bd67ff5fad4949dc9f0da15cf049fd4
Unknown <akhil@zuru.tech>
2018-12-13 18:39:10 +0530

    feat: Day 1 End

--- chatbot/src/Components/ChatBox.jsx
+++ chatbot/src/Components/ChatBox.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from "react";
 
 export default class ChatBox extends Component {
-  // constructor(){
-  //   super()
+  constructor() {
+    super();
+    this.state = {
+      inputValue: "111111"
+    };
 
-  //   this.state{
-  //     chatBoxClass :
-  //   }
-  // }
+    this.enterKeyPressed = this.enterKeyPressed.bind(this);
+  }
+
+  enterKeyPressed(e) {
+    if (e.key === "Enter") {
+      this.setState({
+        inputValue: e.target.value
+      });
+    }
+  }
+
   render() {
     return (
       <div
         className={`chatbox ${this.props.buttonActive ? "" : "chatbox-hidden"}`}
       >
-        1
+        <div className="chatbox-container">
+          <div className="chat-view">{this.state.inputValue}</div>
+
+          <input
+            type="text"
+            placeholder="Start a Converstation"
+            className="chat-textbox"
+            onKeyPress={this.enterKeyPressed}
+          />
+        </div>
       </div>
     );
   }
